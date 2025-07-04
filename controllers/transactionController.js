489a6320ad90a3933969f1bb0c0f4e545c9e2ccf
jesus24e <jesusrodriguez65@aragon.unam.mx>
2025-07-03 19:31:28 -0600

    se implemento el manejo de excepciones basicas en el transaction controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -16,6 +16,10 @@ class TransactionController {
       const transaccion = { ...defaults, ...req.body };
       const data = await transactionRepository.create(transaccion);
 
+      if (!data) {
+        res.status(404).send("error: " + error.message);
+      }
+
       res.status(201).json(data);
       console.log("mesaje recibido en indice " + JSON.stringify(data));
     } catch (error) {
@@ -39,9 +43,12 @@ class TransactionController {
       const { id } = req.params;
       const data = await transactionRepository.getOne(id);
 
+      if (!data) {
+        return res.status(404).send("transaccion no encontrada.");
+      }
       res.status(200).json(data);
     } catch (error) {
-      res.status(404).send(error);
+      res.status(500).send(error);
       console.log(error);
     }
   }
@@ -50,6 +57,14 @@ class TransactionController {
     try {
       const { id } = req.params;
       const data = await transactionRepository.update(id, req.body);
+
+      if (!data) {
+        return res.status(404).send("transaccion no encontrada.");
+      }
+
+      if (!req.body || Object.keys(req.body).length == 0) {
+        return res.status(204).send("body sin contenido");
+      }
 
       res.status(200).json(data);
     } catch (error) {
@@ -62,9 +77,13 @@ class TransactionController {
     try {
       const { id } = req.params;
       const data = await transactionRepository.delete(id);
-      res.json({ msg: `delete transaction ${id}` });
+      if (!data) {
+        return res.status(404).send("transaccion no encontrada.");
+      }
+      // res.status(200).send(`id ${id} eliminado`);
+      res.status(200);
     } catch (error) {
-      res.status(500).send(error);
+      res.status(500).send(`error: ${error.message}`);
       console.log(error);
     }
   }

60a8836254c9afbd98186da0e7ef391c8484de0a
jesus24e <jesusrodriguez65@aragon.unam.mx>
2025-07-13 19:32:52 -0600

    actualizaciones a las interfaz del usuario e implementacion de algunas utilidades extras

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,3 @@
-const API_URL ="https://app-finanzas-fvx3.onrender.com";
+//const API_URL ="https://app-finanzas-fvx3.onrender.com";
+
+const API_URL = "http://localhost:3000"
--- frontend/public/transaction.js
+++ frontend/public/transaction.js
@@ -13,15 +13,26 @@ async function cargarTransacciones() {
   container.innerHTML = "";
 
   data.forEach(t => {
+    let fecha = new Date(t.updatedAt);
+    let fechaStr = fecha.toLocaleDateString('es-MX', {timeZone: 'America/Mexico_City'});
+    let horaStr = fecha.toLocaleTimeString('es-MX', {timeZone: 'America/Mexico_City', hour: '2-digit', minute: '2-digit', hour12: true});
     const div = document.createElement("div");
-    div.className = "col-md-6";
+    div.className = "col-md-4";
 
     div.innerHTML = `
       <div class="card shadow-sm text-bg-light bg-gradient">
         <div class="card-body">
-          <h5 class="card-title text-capitalize">${t.tipo}</h5>
-          <p class="card-text"><strong>Monto:</strong> $${t.monto.toFixed(2)}</p>
-          <p class="card-text"><strong>Descripción:</strong> ${t.descripcion}</p>
+          <h5 class="card-title text-capitalize text-${t.tipo == "gasto" ? "danger":"success"}">${t.tipo}</h5>
+          <p class="card-text"><strong>Monto:</strong> $${formatearConComas(t.monto.toFixed(2))}</p>
+          <p class="card-text">
+            <strong>
+              Descripción:
+            </strong>
+            ${t.descripcion}
+          </p>
+          <p class="card-text">
+            ${fechaStr} | ${horaStr}
+          </p>
           <div class="d-flex justify-content-end gap-2">
             <button class="btn btn-sm btn-outline-primary" onclick="editar('${t._id}')">Editar</button>
             <button class="btn btn-sm btn-outline-danger" onclick="eliminar('${t._id}')">Eliminar</button>
@@ -40,7 +51,6 @@ async function obtenerTotal(tipoTransaccion){
   if (!res.ok) return alert("Error al cargar transacciones");
 
   const data = await res.json();
-  console.log(data)
   const container = document.getElementById(`totalTransacciones${tipoTransaccion}`);
 
   let total=0
@@ -54,10 +64,43 @@ async function obtenerTotal(tipoTransaccion){
   const div = document.createElement("div");
     
     div.innerHTML = `
-          <h5 class="text-${tipoTransaccion === 'Ingreso' ? 'success' : 'danger'}">${tipoTransaccion}s totales</h5>
-          <p><strong>Monto:</strong> $${total.toFixed(2)}</p>
+          <h5 class="text-${tipoTransaccion === 'Ingreso' ? 'success' : 'danger'} fs-3">${tipoTransaccion}s totales</h5>
+          <p class = "fs-5"><strong>Monto:</strong> $${formatearConComas(total.toFixed(2))}</p>
     `;
     container.appendChild(div);
+}
+
+async function obtenerRestante() {
+    const res = await fetch(`${API_URL}/transactions`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  if (!res.ok) return alert("Error al cargar transacciones");
+
+  const data = await res.json();
+  const container = document.getElementById(`restanteTotal`);
+
+  let totalGasto=0;
+  let totalIngreso=0;
+
+  for(const t of data){
+    if(t.tipo=="gasto"){
+      totalGasto+=t.monto
+    }
+    if(t.tipo=="ingreso"){
+      totalIngreso+=t.monto
+    }
+  }
+
+  let restante = totalIngreso-totalGasto;
+  console.log(restante)
+  container.innerHTML = "";
+  const div = document.createElement("div");
+    
+    div.innerHTML = `
+          <h5 class="text-primary fs-3">Restante total</h5>
+          <p class="text-${restante < 0 ? "danger":"succes"} fs-5"><strong>Monto:</strong> $${formatearConComas(restante.toFixed(2))}</p>
+    `;
+  container.appendChild(div);
 }
 
 async function eliminar(id) {
@@ -83,7 +126,15 @@ function logout() {
   location.href = "../index.html";
 }
 
-cargarTransacciones();
-obtenerTotal("Gasto");
-obtenerTotal("Ingreso");
-      
+function cargarDatosTransacciones(){
+  cargarTransacciones();
+  obtenerTotal("Gasto");
+  obtenerTotal("Ingreso");
+  obtenerRestante();
+}
+
+function formatearConComas(numero) {
+  return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+cargarDatosTransacciones();      
